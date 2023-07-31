c55755cdf3c82980107aaf43903a2918fe091cee
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-07-30 22:40:43 -0300

    Aprofundando no MVC - A vez do New e Create - Ruby on Rails

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,8 +7,25 @@ class ProductsController < ApplicationController
 
   def show; end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_url
+    else
+      render :new, :unprocessable_entity
+    end
+  end
+
   private
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :quantidade, :price)
   end
 end
